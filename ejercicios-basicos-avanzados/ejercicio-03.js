305fbad0a3965b8504ef2c39ac9e6b3d43032f82
Cecilia <cecilia.delacamara@gmail.com>
2025-12-28 14:19:07 +0100

    ejercicio 3

---
+++ ejercicios-basicos-avanzados/ejercicio-03.js
@@ -0,0 +1,11 @@
+//3. Calculando el coste total de sables de luz:
+//Obtén el precio total de dos sables de luz: "Shoto de Yoda" y "sable de Darth Vader".
+//Imprime el resultado por consola utilizando console.log.
+const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
+const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
+
+// Tu código para calcular el precio total aquí...
+
+let precioTotal = sable1.precio + sable2.precio
+
+console.log(precioTotal)
