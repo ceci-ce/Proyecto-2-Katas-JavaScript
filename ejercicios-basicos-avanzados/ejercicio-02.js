e047efe13c68b507e9cdeec4f438a627089b6c3d
Cecilia <cecilia.delacamara@gmail.com>
2025-12-28 14:13:37 +0100

    ejercicio 2

---
+++ ejercicios-basicos-avanzados/ejercicio-02.js
@@ -0,0 +1,23 @@
+//1. Luke Skywalker cumple años:
+//Modifica el siguiente código JavaScript para actualizar la edad de Luke Skywalker a 25 años.
+const jedi = {nombre: "Luke Skywalker", edad: 19};
+
+// Tu código para cambiar la edad aquí... 
+
+jedi.edad = 28;
+
+//2. Presentación al estilo Leia Organa:
+//Crea tres variables con la siguiente información:
+//nombre: "Leia"
+//apellido: "Organa"
+//edad: 20
+
+let nombre = "Leia";
+let apellido = "Organa";
+let edad = 20;
+
+//Muestra un mensaje por consola que siga la siguiente estructura:
+//"Soy Leia Organa, tengo 20 años y soy una princesa de Alderaan."
+//Utiliza la concatenación para ello.
+
+console.log("Soy " + nombre + " " + apellido + ", tengo " + edad + " años y soy una princesa de Alderaan." )
