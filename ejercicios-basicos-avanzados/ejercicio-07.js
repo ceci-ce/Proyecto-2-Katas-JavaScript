9e31ef3e697039470f212cbcbdf3dc5cb883d5be
Cecilia <cecilia.delacamara@gmail.com>
2025-12-29 18:58:01 +0100

    ejercicio 7

---
+++ ejercicios-basicos-avanzados/ejercicio-07.js
@@ -0,0 +1,13 @@
+//Completa esta función para que, al recibir dos números por argumento, te devuelva por consola el más alto de los dos.
+
+function greaterNumber(numberOne , numberTwo) {
+  if(numberOne > numberTwo){
+    return numberOne
+  }else{
+    return numberTwo
+  }
+}
+
+console.log(greaterNumber(2,6));
+console.log(greaterNumber(8,7));
+
