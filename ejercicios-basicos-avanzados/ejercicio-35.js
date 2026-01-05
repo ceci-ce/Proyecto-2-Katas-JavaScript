9834841d898f1390ea30f981ba73da9d1099723c
Cecilia <cecilia.delacamara@gmail.com>
2026-01-05 19:59:06 +0100

    ejercicio 35

---
+++ ejercicios-basicos-avanzados/ejercicio-35.js
@@ -0,0 +1,38 @@
+//Desarrolla una función que busque en un array de objetos representando mutantes si existe alguno con un poder específico y retorne un mensaje indicando
+//si fue encontrado o no.
+
+//Considera el caso de múltiples mutantes con el mismo poder.
+
+const mutants = [
+  { name: 'Wolverine', power: 'regeneration' },
+  { name: 'Magneto', power: 'magnetism' },
+  { name: 'Professor X', power: 'telepathy' },
+  { name: 'Jean Grey', power: 'telekinesis' },
+  { name: 'Rogue', power: 'power absorption' },
+  { name: 'Storm', power: 'weather manipulation' },
+  { name: 'Mystique', power: 'shape-shifting' },
+  { name: 'Beast', power: 'superhuman strength' },
+  { name: 'Colossus', power: 'steel skin' },
+  { name: 'Nightcrawler', power: 'teleportation' }
+];
+
+function findMutantByPower(mutants, power) {
+    let count = 0;
+
+    for(const mutant of mutants){
+        if(mutant.power === power){
+            count++;
+        }
+    }
+
+    if(count > 0){
+        return `Se encontraron ${count} mutante(s) con el poder "${power}".`;
+    }else{
+            return `No se encontró ningún mutante con el poder "${power}".`;
+        }
+    }
+    
+    console.log(findMutantByPower(mutants,'shape-shifting'));
+    console.log(findMutantByPower(mutants, 'telepathy'));
+    console.log(findMutantByPower(mutants, 'regeneration'));
+    console.log(findMutantByPower(mutants, 'flight'));
